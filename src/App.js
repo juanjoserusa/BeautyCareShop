b5a7ac1b11e48362583c616ea43259da0efb821d
Juan Jose Ruiz Sanchez <juanjoserusa@gmail.com>
2023-01-05 13:01:37 +0100

    creacion proyecto y primeras vista de la pagina principal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import Header from "./components/header/Header";
+import Hero from "./components/hero/Hero";
+import Slider from "./components/slider/Slider";
 
 function App() {
   return (
     <div className="App">
-      <span>Subscribe Zainkeepscode</span>
+      <Header />
+      <Hero />
+      <Slider />
     </div>
   );
 }
